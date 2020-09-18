3ccb26bbb790b3e7921022b82c6650c95678a61e
Hossam <Matrixhoss2@gmail.com>
2020-09-18 21:50:30 +0200

    create ios tabs

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View  ,Platform} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStore ,applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers'
@@ -14,7 +15,8 @@ import AddCard from './components/AddCard'
 import QuizView from './components/QuizView'
 
    const Stack = createStackNavigator()
-   const Tab = createMaterialTopTabNavigator()
+   const Tab = Platform.OS==='ios' ? createBottomTabNavigator() :createMaterialTopTabNavigator()
+  
 const Tabs = () => {
   return (
     <Tab.Navigator  
